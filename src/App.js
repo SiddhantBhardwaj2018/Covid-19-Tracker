2435be8a165427a20aed3f6a16f2466bb699259e
Siddhant Bhardwaj <bhardwajsiddhant03@gmail.com>
2020-11-19 01:20:12 +0530

    Commiting Covid-Tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  CardContent,
+  Card,
+} from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import Table from "./Table";
+import { sortData } from "./utils";
+import LineGraph from "./LineGraph";
+import "./Outline.css";
+import "leaflet/dist/leaflet.css";
+import {  prettyPrintStat } from './utils'
+import numeral from 'numeral'
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setInputCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(2);
+  const [casesType, setCasesType] = useState("cases");
+  const [mapCountries,setmapCountries] = useState([])
+
+  //https://disease.sh/v3/covid-19/countries
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setmapCountries(data)
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setInputCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setInputCountry(countryCode);
+        setCountryInfo(data);
+        if(data.countryInfo){
+          setMapCenter({lat: data.countryInfo.lat,lng:  data.countryInfo.long});
+          setMapZoom(3.5);
+        }else{
+          setMapCenter({  lat: 34.80746, lng: -40.4796  })
+          setMapZoom(2)
+        }
+          
+      });
+
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER APP</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+        <InfoBox
+            onClick={(e) => setCasesType("cases")}
+            title="Coronavirus Cases"
+            isRed
+            active={casesType === "cases"}
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={numeral(countryInfo.cases).format("0.0a")}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType("recovered")}
+            title="Recovered"
+            active={casesType === "recovered"}
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={numeral(countryInfo.recovered).format("0.0a")}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType("deaths")}
+            title="Deaths"
+            isRed
+            active={casesType === "deaths"}
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={numeral(countryInfo.deaths).format("0.0a")}
+          />
+        </div>
+
+        <Map countries = {mapCountries} casesType = {casesType} center={mapCenter} zoom={mapZoom} />
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3 className="outline">Worldwide Cases</h3>
+          <LineGraph className = "app__graph" casesType={casesType} />
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyB6VuvueQJALm-iQOgyAo5byh_fHX4slE4",
+  authDomain: "covid-19-tracker-62f79.firebaseapp.com",
+  databaseURL: "https://covid-19-tracker-62f79.firebaseio.com",
+  projectId: "covid-19-tracker-62f79",
+  storageBucket: "covid-19-tracker-62f79.appspot.com",
+  messagingSenderId: "931412803994",
+  appId: "1:931412803994:web:bfb492dab4246013517a23",
+  measurementId: "G-N061E1GVPE",
+});
